00aa17283f8819baec89cc6685b930c253e8a066
Liza <skomorohova-elizaveta@rambler.ru>
2022-07-06 13:53:07 +0300

    Popolnenie

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Здравствуйте!");
+        System.out.println("Введите сумму средств на счете до пополнения и сумму пополнения");
+
+        int summaPopolnenia = 0;
+        int accountАmount = 0;
+        int schet = summaPopolnenia + accountАmount;
+        int bonus = schet / 100;
+        if (schet < 1000) {
+            System.out.println("Для начисления бонусных рублей на счете должо быть не менее 1000 рублей");
+        } else {
+            System.out.println(bonus+schet +" рублей");
+        }
+        System.out.println("До свидания!");
+    }
+}
